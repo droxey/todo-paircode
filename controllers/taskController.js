db04d5b36757554d67a0190b8667ab75ddf04803
donaldvallejo <donny.vallejo@gmail.com>
2017-11-20 21:24:18 -0800

    added put delete and create

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -2,7 +2,6 @@
 // =============================================================
 var db = require("../models");
 
-
 // Routes
 // =============================================================
 module.exports = function(app) {
@@ -15,25 +14,39 @@ module.exports = function(app) {
     });
   });
 
-  // Add a chirp
   app.post("/todo/new", function(req, res) {
     db.TodoItem.create({
       item_text: req.body.item_text
-    }).then(function(results) {
+    }).then(function(todo_db) {
       // `results` here would be the newly created chirp
-      res.json(results);
+      res.json(todo_db);
     });
-  });  
+  });
 
   // PUT route for updating todos. The updated todo will be available in req.body
   app.put("/todo/:id", function(req, res) {
+    db.todoItem.update({
+      text: req.body.item_text,
+      complete: req.body.is_complete
+    }).then(function(todo_db) {
+      // We have access to the new todo as an argument inside of the callback function
+      res.json(todo_db);
+    })
+    .catch(function(err) {
+     // Whenever a validation or flag fails, an error is thrown
+     // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+     res.json(err);
+   });
+ });
 
-  });
-  
   // DELETE route for deleting todos. You can access the todo's id in req.params.id
   app.delete("/todo/:id", function(req, res) {
-
-  });
-
-
+    db.todoItem.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(todo_db) {
+      res.json(todo_db);
+    });
+    })
 };
